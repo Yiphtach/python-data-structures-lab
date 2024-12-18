06b9874603f39f373ee4ef012f78eff329b3ee6e
jephthe <jephthepetitfrere@gmail.com>
2024-12-17 20:09:50 -0500

    exercise 3

--- exercises.py
+++ exercises.py
@@ -56,3 +56,34 @@ def manage_students_function():
 # Call the function and print the result
 print('Exercise 1:', manage_students_function())
 
+
+# Exercise 2: Loop and String Concatenation
+#
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a variable named meal and assign an empty string to it.
+# Use a for loop to iterate over the strings in foods and append each string to meal.
+
+def combine_foods():
+    # your code here
+    foods = ['chicken','rice', 'beans']
+    meal = ()
+    for food in foods:
+        meal += (food,)
+    return meal
+
+# Call the function and print the result
+print('Exercise 2:', combine_foods())
+
+
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+    # your code here
+    foods = ('chicken','rice', 'beans')
+    last_two_foods = foods[-2:]
+    return last_two_foods
+
+# Call the function and print the result
+print('Exercise 3:', slice_foods())
